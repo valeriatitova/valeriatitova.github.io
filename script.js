efa4916509d80eea0dde1aa3f7a13c53db6b8f18
valeriatitova <37398424+valeriatitova@users.noreply.github.com>
2019-04-03 21:22:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ var btn= document.getElementsByTagName('button');
 
 
 btn[0].addEventListener('click', function(event) {
-  alert('Произошло событие' + event.type + 'на элементе' + event.target);
+  alert('Произошло событие  ' + event.type + ' на элементе  ' + event.target);
 });
