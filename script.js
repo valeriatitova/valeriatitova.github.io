8c5ead4597f28d5be27236c6a592d1538feadb97
valeriatitova <37398424+valeriatitova@users.noreply.github.com>
2019-04-03 21:27:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ btn[0].addEventListener('click', function(event) {
   target.style.display = 'none';
   //alert('Произошло событие  ' + event.type + ' на элементе  ' + event.target);
 });
+
+// lottie
+var svgContainer = document.getElementById('test');
+var animItem = document.getElementById('test');
+
+var animItem = lottie.loadAnimation({
+  wrapper: svgContainer,
+  animType: 'svg',
+  loop: true,
+  path: 'https://valeriatitova.github.io/tesst.json' // the path to the animation json);
+});
